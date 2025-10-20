3e43ea104aaffd556ce507b024a47520eb672367
stephenp0320 <stephen power>
2025-10-20 10:35:25 +0100

    Initial commit

---
+++ movies/src/contexts/moviesContext.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [favorites, setFavorites] = useState( [] )
+  const [myReviews, setMyReviews] = useState( {} ) 
+  const [mustWatch, setMustWatch] = useState([]);
+
+
+  const addToFavorites = (movie) => {
+    let newFavorites = [];
+    if (!favorites.includes(movie.id)){
+      newFavorites = [...favorites, movie.id];
+    }
+    else{
+      newFavorites = [...favorites];
+    }
+    setFavorites(newFavorites)
+  };
+
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+  
+
+  const removeFromFavorites = (movie) => {
+    setFavorites( favorites.filter(
+      (mId) => mId !== movie.id
+    ) )
+  };
+
+  const addToMustWatch = (movie) => {
+    if(!mustWatch.includes(movie.id)) {
+      const updated = [...mustWatch, movie.id];
+      setMustWatch(updated)
+      console.log("list of must watch, ", updated)
+    } else {
+      console.log("already in must watch list, ", movie.id);
+    }
+  };
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        addReview,
+        mustWatch,
+        addToMustWatch,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+
+};
+
+export default MoviesContextProvider;
